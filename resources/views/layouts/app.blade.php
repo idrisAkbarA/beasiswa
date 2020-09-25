2b98bfe97595da36303172d5ec161d45bf7962f8
gaskanaja <gaskanajaguys@gmail.com>
2020-09-25 20:17:32 +0700

    :hammer: Change Title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
-    <title>Vue SPA Demo</title>
+    <title>Beasiswa</title>
 </head>
 <body>
     <div id="app">
